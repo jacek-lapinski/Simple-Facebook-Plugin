86110ad52a8550a8706f2a486f7dc931813fcaa0
Jacek Łapiński <Jacek apiski>
2016-10-11 23:23:31 +0200

    temp

--- src/fsg.js
+++ src/fsg.js
@@ -1,33 +1,19 @@
-
-window.fbAsyncInit = function () {
-  $.getScript("http://connect.facebook.net/en_US/sdk.js")
-    .done(function (script, textStatus) {
-      //console.log("Facebook Init called..");
-      FB.init({
-        appId: '1270801512983487',
-        cookie: true,
-        xfbml: false,
-        version: 'v2.8'
-      });
-      FB.AppEvents.logPageView();
-      FB.getLoginStatus(function (response) {
-        if (response.status === 'connected') {
-          console.log('Logged in.');
-        }
-        else {
-          FB.login();
-        }
-      });
-    })
-    .fail(function (jqxhr, settings, exception) {
-      //console.log("Facebook Init failed..");
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1270801512983487',
+      xfbml      : true,
+      version    : 'v2.8'
     });
-};
 
-(function (d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) { return; }
-  js = d.createElement(s); js.id = id;
-  js.src = "http://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-} (document, 'script', 'facebook-jssdk'));
+    FB.api('/kendosopot/albums', { access_token: 'EAACEdEose0cBAO8UPWdlGZCLiWuRmeB7jrZCm6TFUk96xtZAZCoXUDW5oFV86490HcoMU4rTUbVH7QAfF4g5MuZAyy1lWgC5c8Csa1ETZCWJQggSolr5MwDyB8NDYJkoV2E3zZAiiiZC5oZBJPF6bOKaxa3ZAms2sLeRGIvnqCHHDRaAZDZD'}, function(response) {
+        console.log(response);
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "http://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
